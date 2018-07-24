43ab993346b1d3210ab002822693d14b1b2f6eaa
egen111222 <egen13@ukr.net>
2018-07-24 15:37:19 +0300

    Update models.py

--- master/models.py
+++ master/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 class Master(models.Model):
-	name 		= models.CharField(max_length=40,null=False)
+	name 		= models.CharField(max_length=40)
 	experience  	= models.IntegerField()
 
 	def __str__(self):
